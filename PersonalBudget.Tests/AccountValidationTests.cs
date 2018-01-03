463f08860fcc865a82f7e8a4a3202b2b87f84755
Graham MacMaster <graham.macmaster@gmail.com>
2018-01-03 10:05:38 -0500

    Add validation unit tests for Account

---
+++ PersonalBudget.Tests/AccountValidationTests.cs
@@ -0,0 +1,24 @@
+﻿using PersonalBudget.Models;
+using PersonalBudget.Tests.Helpers;
+
+using Xunit;
+
+namespace PersonalBudget.Tests
+{
+    public sealed class AccountValidationTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("\t")]
+        [InlineData(" ")]
+        public void ValidateAccount_WithInvalidName_FailsValidation(string name)
+        {
+            var account = new Account { Name = name };
+
+            var validationResults = ValidationHelpers.ValidateModel(account);
+
+            Assert.True(validationResults.Count == 1);
+        }
+    }
+}
