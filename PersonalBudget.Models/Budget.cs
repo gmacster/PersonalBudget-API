d7e1665dc2dab333171bfb2f8d9f8df724d73905
Graham MacMaster <graham.macmaster@gmail.com>
2018-03-11 12:04:25 -0400

    Add budget entity and controller

---
+++ PersonalBudget.Models/Budget.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using PersonalBudget.Models.Interfaces;
+
+namespace PersonalBudget.Models
+{
+    /// <summary>
+    /// Represents a container for all associated accounts, categories, transactions, etc.
+    /// </summary>
+    public sealed class Budget : IEntity
+    {
+        /// <summary>
+        /// Gets or sets the primary key for this <see cref="Budget"/>.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets a friendly name for this <see cref="Budget"/>.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ PersonalBudget/Controllers/BudgetsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using PersonalBudget.Controllers.Common;
+using PersonalBudget.Models;
+
+namespace PersonalBudget.Controllers
+{
+    [Route("api/Budgets")]
+    public class BudgetsController : EntityController<Budget>
+    {
+        public BudgetsController(IUnitOfWork unitOfWork)
+            : base(unitOfWork)
+        {
+        }
+    }
+}
